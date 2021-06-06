0d34d7b3eba6e1476424e1be855cb9e3d30afdc5
akhilgangidi <arg170030@utdallas.edu>
2021-06-06 17:43:08 -0500

    Implemented Navigation Bar with routes set up

--- App.js
+++ App.js
@@ -1,21 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StyleSheet, Text, View } from 'react-native';
-
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-home'
+                : 'ios-home-outline';
+            } else if (route.name === 'Search') {
+              iconName = focused ? 'ios-search' : 'ios-search-outline'
+            } else if (route.name === 'Settings') {
+              iconName = focused ? 'ios-settings' : 'ios-settings-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+        >
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="Search" component={SearchScreen}/>
+        <Stack.Screen name="Settings" component={SettingsScreen}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
+//placeholder
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}
 
+//placeholder
+function SettingsScreen() {
+  return( 
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Settings Screen</Text>
+    </View>
+  )
+}
+
+//placeholder
+function SearchScreen() {
+  return(
+    <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Search Screen</Text>
+    </View>
+  )
+}
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    paddingTop: 40,
+    backgroundColor: '#ddd',
   },
-});
+  navBar: {
+    backgroundColor: 'skyblue',
+    padding: 33
+  },
+  contentContainer: {
+    flex: 1
+  }
+})
